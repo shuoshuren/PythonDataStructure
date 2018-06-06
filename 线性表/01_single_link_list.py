ece52514c777d87a8f437ceccadf976e4c3a1419
shuoshuren <534361124@qq.com>
2018-06-06 23:25:56 +0800

    单链表的实现

--- 线性表/01_single_link_list.py
+++ 线性表/01_single_link_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # coding:utf-8
 
 
@@ -14,38 +15,134 @@ class SingleLinkList(object):
 	'''单链表'''
 
 	def __init__(self,node = None):
-		self._head = node
+		self.__head = node
 
 
 	def is_empty(self):
 		"""链表是否为空"""
-		pass
+		return self.__head == None
 
-	def lenght(self):
+	def length(self):
 		"""链表长度"""
+		# cur游标，用来移动遍历节点
+		cur = self.__head
+		# count 用来计数
+		count = 0
+		while cur != None:
+			count +=1
+			cur = cur.next
+
+		return count
 		pass
 
 	def travel(self):
 		"""遍历整个链表"""
-		pass
+		cur = self.__head
+		while cur != None:
+			print(cur.elem,end=" ")
+			cur = cur.next
+		print("")
+		
 
 	def add(self,item):
 		"""链表头部添加元素"""
-		pass
+		node = Node(item)
+		node.next = self.__head
+		self.__head = node
+
 
 
 	def append(self,item):
 		'''链表尾部添加元素'''
-		pass
+		node = Node(item)
+		if self.is_empty():
+			self.__head = node
+		else:
+			cur = self.__head
+			while cur.next != None:
+				cur = cur.next
+			cur.next = node
+		
 
 	def insert(self,pos,item):
-		"""指定位置添加元素"""
-		pass
+		"""
+		指定位置添加元素
+		:param pos 从0开始
+		"""
+		if pos <= 0:
+			self.add(item)
+		elif pos > (self.length()-1):
+			self.append(item)
+		else:
+			node = Node(item)
+			count = 0
+			pre = self.__head
+			while count < (pos-1):
+				count+=1
+				pre = pre.next
+				# 当循环退出后，pre指向pos-1的位置
+			node.next = pre.next
+			pre.next = node
+
 
 	def remove(self,item):
 		"""删除节点"""
-		pass
+		cur = self.__head
+		pre = None
+		while cur != None:
+			if cur.elem == item:
+				# 先判断此节点是否是头节点
+				if cur == self.__head:
+					self.__head = cur.next
+				else:
+					pre.next = cur.next
+					cur.next = None
+				break
+			else:
+				pre = cur
+				cur = cur.next
 
 	def search(self,item):
 		"""查找节点是否存在"""
-		pass
+		cur = self.__head
+		while cur != None:
+			if cur.elem == item:
+				return True
+			else:
+				cur = cur.next
+		return False
+
+
+
+
+if __name__ == '__main__':
+	
+	ll = SingleLinkList()
+	print(ll.is_empty())
+	print(ll.length())
+
+	ll.append(1)
+	print(ll.is_empty())
+	print(ll.length())
+
+	ll.append(2)
+	ll.add(8)
+	ll.append(3)
+	ll.append(4)
+	ll.append(5)
+	ll.append(6)
+	ll.travel()
+	ll.insert(-1,9) 
+	ll.travel() # 9 8 1 2 3 4 5 6
+	ll.insert(2,100) 
+	ll.travel() # 9 8 1 100 2 3 4 5 6
+	ll.insert(10,200)
+	ll.travel() # 9 8 1 100 2 3 4 5 6 200
+	ll.remove(200)
+	ll.travel()
+	ll.remove(100)
+	ll.travel()
+	ll.remove(9)
+	ll.travel()
+	print(ll.search(6))
+	print(ll.search(200))
