24d18cd77f9e2171dce302997b146adb2a6f3017
shuoshuren <534361124@qq.com>
2018-06-08 21:32:18 +0800

    栈的实现

---
+++ 栈和队列/01_stack.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# coding:utf-8
+
+
+class Stack(object):
+	"""栈"""
+
+	def __init__(self):
+		self.__list = []
+
+	def push(self,item):
+		"""添加一个新打元素item到栈顶"""
+		self.__list.append(item)
+
+
+	def pop(self):
+		"""弹出栈顶元素"""
+		return self.__list.pop()
+
+	def peek(self):
+		"""返回栈顶元素"""
+		if self.is_empty():
+			return None
+		else:
+			return self.__list[-1]
+		
+
+	def is_empty(self):
+		"""判断栈是否为空"""
+		return self.size() == 0
+
+	def size(self):
+		"""返回栈中元素个数"""
+		return len(self.__list)
+
+
+
+if __name__ == '__main__':
+	
+	s = Stack()
+	s.push(1)
+	s.push(2)
+	s.push(3)
+	s.push(4)
+	print(s.pop())
+	print(s.pop())
+	print(s.pop())
+	print(s.pop())
+
+
